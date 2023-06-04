b14bde24756daf7136dc8372004f15b11c5f9429
Nogro <dimnogro@gmail.com>
2023-06-04 11:25:42 -0300

    publish config

--- Edi.Rest/Program.cs
+++ Edi.Rest/Program.cs
@@ -6,7 +6,6 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 
 builder.Services.AddControllers();
-
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen(c =>
@@ -25,14 +24,7 @@ if (app.Environment.IsDevelopment())
     app.UseSwagger();
     app.UseSwaggerUI();
 }
-
-app.UseHttpsRedirection();
-
-app.UseAuthorization();
-
 app.MapControllers();
-
-
 
 using (var serviceScope = app.Services.CreateScope())
 {
@@ -41,4 +33,4 @@ using (var serviceScope = app.Services.CreateScope())
 }
 
 
-app.Run();
+await app.RunAsync();

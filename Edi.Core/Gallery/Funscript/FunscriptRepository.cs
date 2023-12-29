f6333afed97da0b618ef3ef6d239a851e6cdd274
Toast <>
2023-12-29 14:42:25 +0200

    Variant naming fixes

--- Edi.Core/Gallery/Funscript/FunscriptRepository.cs
+++ Edi.Core/Gallery/Funscript/FunscriptRepository.cs
@@ -55,7 +55,8 @@ namespace Edi.Core.Gallery.CmdLineal
 
             foreach (var funscript in funscriptsFiles)
             {
-                var pathVariant = funscript.path.Replace(GalleryDir.FullName, "").Split('\\')[0];
+                var pathSplit = funscript.path.Replace(GalleryDir.FullName + "\\", "").Split('\\');
+                var pathVariant = pathSplit.Length > 1 ? pathSplit[0] : null;
                 funscript.variant = !string.IsNullOrEmpty(funscript.variant)
                                         ? funscript.variant
                                         : pathVariant ?? Config.DefaulVariant;

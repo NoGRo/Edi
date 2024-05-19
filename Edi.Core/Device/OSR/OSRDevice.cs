7a320a69a69b599ebe3aaeb797afbf42afd7bd07
Toast <>
2024-05-19 09:22:57 +0200

    Swop out interface for DeviceBase inheritance

--- Edi.Core/Device/OSR/OSRDevice.cs
+++ Edi.Core/Device/OSR/OSRDevice.cs
@@ -23,97 +23,73 @@ using System.Data;
 using NAudio.CoreAudioApi;
 using FunscriptIntegrationService.Connector.Shared;
 using System.Collections.Concurrent;
+using System.Transactions;
+using System.Threading;
 
 namespace Edi.Core.Device.OSR
 {
     [AddINotifyPropertyChangedInterface]
-    public class OSRDevice : IDevice
+    public class OSRDevice : DeviceBase<FunscriptRepository,  FunscriptGallery>
     {
         public SerialPort DevicePort { get; private set; }
-        public string Name { get; set; }
         public OSRConfig Config { get; private set; }
-        public string SelectedVariant
-        {
-            get => selectedVariant;
-            set
-            {
-                selectedVariant = value;
-                if (currentGallery != null && playbackScript != null && !IsPause)
-                    PlayGallery(currentGallery.Name, playbackScript.CurrentTime).GetAwaiter();
-            }
-        }
-        public IEnumerable<string> Variants => repository.GetVariants();
         public DateTime GalleryStart { get; private set; } = DateTime.Now;
-        public bool IsPause { get; private set; } = true;
-        public bool IsReady => DevicePort?.IsOpen == true;
         public CmdLinear? LastCommandSent(Axis axis) => lastCommandSent.ContainsKey(axis) ? lastCommandSent[axis] : null;
 
 
         private readonly string[] channelCodes = new string[] { "L0", "L1", "L2", "R0", "R1", "R2", "V0", "A0", "A1" };
-        private FunscriptRepository repository { get; set; }
-        private string selectedVariant;
 
-
-        private FunscriptGallery? currentGallery;
-        private OSRScript? playbackScript { get; set; }
-        private bool isNewScript { get; set; } = false;
-        private bool speedRampUp { get; set; } = false;
-        private DateTime? speedRampUpTime { get; set; }
+        private OSRScript? PlaybackScript { get; set; }
+        private bool IsNewScript { get; set; } = false;
+        private bool SpeedRampUp { get; set; } = false;
+        private DateTime? SpeedRampUpTime { get; set; }
         private ConcurrentDictionary<Axis, CmdLinear> lastCommandSent { get; set; } = new();
         private volatile CancellationTokenSource playbackCancellationTokenSource = new();
 
-        private SemaphoreSlim semaphore = new(1, 1);
-
+        private SemaphoreSlim AsyncLock = new(1, 1);
         private string ChannelCode(Axis type) => channelCodes[(int)type];
 
-        public OSRDevice(SerialPort devicePort, FunscriptRepository repository, OSRConfig config)
+        public OSRDevice(SerialPort devicePort, FunscriptRepository repository, OSRConfig config) : base(repository)
         {
             DevicePort = devicePort;
             Name = GetDeviceName();
 
             Config = config;
-            this.repository = repository;
-
-            selectedVariant = repository.GetVariants().FirstOrDefault();
         }
 
-        public async Task PlayGallery(string name, long seek = 0)
+        override public async Task PlayGallery(FunscriptGallery gallery, long seek = 0)
         {
-            var gallery = repository.Get(name, SelectedVariant);
-            if (gallery == null)
-                return;
-
             var script = new OSRScript(gallery.AxisCommands, seek);
-            isNewScript = currentGallery?.Name != gallery.Name;
+            IsNewScript = currentGallery?.Name != gallery.Name;
             currentGallery = gallery;
             if (IsPause)
-                speedRampUp = true;
+                SpeedRampUp = true;
 
             IsPause = false;
 
             PlayScript(script);
         }
 
-        public async Task Stop()
+        override public async Task StopGallery()
         {
-            IsPause = true;
-            playbackCancellationTokenSource.Cancel();
+            playbackCancellationTokenSource?.Cancel();
         }
 
         private void PlayScript(OSRScript script)
         {
             var newCancellationTokenSource = new CancellationTokenSource();
-            semaphore.Wait();
+            AsyncLock.Wait();
             try
             {
-                playbackCancellationTokenSource.Cancel();
+                playbackCancellationTokenSource?.Cancel();
                 playbackCancellationTokenSource = newCancellationTokenSource;
-            } finally
+            }
+            finally
             {
-                semaphore.Release();
+                AsyncLock.Release();
             }
 
-            playbackScript = script;
+            PlaybackScript = script;
             script.ProcessCommands(this);
 
             _ = Task.Run(() => PlayCommands(newCancellationTokenSource.Token));
@@ -121,10 +97,10 @@ namespace Edi.Core.Device.OSR
 
         private void PlayCommands(CancellationToken token)
         {
-            if (playbackScript == null)
+            if (PlaybackScript == null)
                 return;
 
-            Axis[] scriptCommandTypes = playbackScript.SupportedAxis.ToArray();
+            Axis[] scriptCommandTypes = PlaybackScript.SupportedAxis.ToArray();
             HashSet<Axis> finishedCommandTypes = new();
 
             while (!token.IsCancellationRequested)
@@ -134,7 +110,7 @@ namespace Edi.Core.Device.OSR
                     if (finishedCommandTypes.Contains(axis))
                         continue;
 
-                    var send = playbackScript.GetNextCommand(axis, out CmdLinear? nextCmd);
+                    var send = PlaybackScript.GetNextCommand(axis, out CmdLinear? nextCmd);
 
                     if (token.IsCancellationRequested || (nextCmd == null && send == true))
                     {
@@ -147,8 +123,8 @@ namespace Edi.Core.Device.OSR
 
                         if (currentGallery?.Loop == true && !token.IsCancellationRequested)
                         {
-                            playbackScript.Loop();
-                            PlayScript(playbackScript);
+                            PlaybackScript.Loop();
+                            PlayScript(PlaybackScript);
                             return;
                         }
 
@@ -159,22 +135,22 @@ namespace Edi.Core.Device.OSR
                     {
 
                         var prevCmd = LastCommandSent(axis);
-                        if (!speedRampUp && nextCmd.Millis <= 200 && isNewScript && prevCmd != null)
+                        if (!SpeedRampUp && nextCmd.Millis <= 200 && IsNewScript && prevCmd != null)
                         {
                             var deltaValue = Math.Abs(prevCmd.Value - nextCmd.Value);
                             var speed = deltaValue / nextCmd.Millis;
 
-                            speedRampUp = speed > 0.4;
+                            SpeedRampUp = speed > 0.4;
                         }
 
-                        isNewScript = false;
-                        if (speedRampUp)
+                        IsNewScript = false;
+                        if (SpeedRampUp)
                         {
-                            if (speedRampUpTime == null)
-                                speedRampUpTime = DateTime.Now;
+                            if (SpeedRampUpTime == null)
+                                SpeedRampUpTime = DateTime.Now;
 
                             var rampUpDuration = 1000;
-                            var adjustment = rampUpDuration - DateTime.Now.Subtract((DateTime)speedRampUpTime).TotalMilliseconds;
+                            var adjustment = rampUpDuration - DateTime.Now.Subtract((DateTime)SpeedRampUpTime).TotalMilliseconds;
 
                             if (adjustment > 0)
                             {
@@ -184,9 +160,9 @@ namespace Edi.Core.Device.OSR
                             }
                             else
                             {
-                                speedRampUp = false;
-                                isNewScript = false;
-                                speedRampUpTime = null;
+                                SpeedRampUp = false;
+                                IsNewScript = false;
+                                SpeedRampUpTime = null;
                             }
                         }
 
@@ -212,7 +188,7 @@ namespace Edi.Core.Device.OSR
         {
             var cmds = ReturnToHomeCommands();
 
-            await semaphore.WaitAsync();
+            await AsyncLock.WaitAsync();
             try
             {
                 cmds.Keys
@@ -222,7 +198,7 @@ namespace Edi.Core.Device.OSR
             }
             finally
             {
-                semaphore.Release();
+                AsyncLock.Release();
             }
         }
 

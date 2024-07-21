de11b3b423759307656d2e522816ba8a90edd97c
Nogro . <dimnogro@gmail.com>
2024-07-21 19:35:57 -0300

    fix

--- Edi.Core/Gallery/Index/GalleryBundlerConfig.cs
+++ Edi.Core/Gallery/Index/GalleryBundlerConfig.cs
@@ -2,7 +2,6 @@
 {
     public class GalleryBundlerConfig
     {
-        public static string Secction = "GalleryBundler";
         public int MinRepearDuration { get; set; } = 7000;
         public int RepearDuration { get; set; } = 2000;
         public int SpacerDuration { get; set; } = 5000;

1384260d616608e9200cd1ca94097068981c8a33
Nogro . <dimnogro@gmail.com>
2024-07-29 01:22:45 -0300

    Comment in irepository

--- Edi.Core/Gallery/IRepository.cs
+++ Edi.Core/Gallery/IRepository.cs
@@ -2,8 +2,22 @@
 {
     public interface IRepository
     {
+
+
         Task Init(string path);
+
+        // File format: {Name}.{variant}.{axis?}.{extension}
+        // Example: Punch-1.simple.sway.funscript 
+        // [Name] Punch-1
+        // [Variant] simple
+        // [Axis?] sway (optional)
+        // [Extension] funscript
+        // Extension is determined based on accept 
+        // Axis is optional and is removed and reserved for the repository to avoid confusion with the variant 
+
+        // File masks or extensions accepted by the repository, e.g., [funscript] or [mp3]
         IEnumerable<string> Accept { get; }
+        // Parameters of the files used by the repository, e.g., [Surge, Sway, Twist]
         IEnumerable<string> Reserve => Array.Empty<string>();
     }
 }

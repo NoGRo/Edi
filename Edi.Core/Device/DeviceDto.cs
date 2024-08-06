cf94b224eada0cb27ebebc84f8b6e0e5a2eef961
dimnogro <Dimnogro@mail.com>
2024-08-06 17:40:01 -0300

    device dto

--- Edi.Core/Device/DeviceDto.cs
+++ Edi.Core/Device/DeviceDto.cs
@@ -3,8 +3,9 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Edi.Core.Device.Interfaces;
 
-namespace Edi.Core.Device.Interfaces
+namespace Edi.Core.Device
 {
     public class DeviceDto : IRange
     {

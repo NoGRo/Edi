77a3a6a821acee71970f86358f30819e6a86bc03
Creek191 <thecreek191@gmail.com>
2025-12-05 16:01:13 +0100

    Create Avalonia project

---
+++ Edi.Avalonia/App.axaml.cs
@@ -0,0 +1,34 @@
+using System;
+using Avalonia;
+using Avalonia.Controls.ApplicationLifetimes;
+using Avalonia.Markup.Xaml;
+using Edi.Core;
+
+namespace Edi.Avalonia;
+
+public partial class App : Application
+{
+    public static IEdi Edi { get; private set; } = null!;
+    public static IServiceProvider ServiceProvider { get; private set; } = null!;
+
+    public App(IServiceProvider serviceProvider)
+    {
+        Edi = edi;
+        ServiceProvider = serviceProvider;
+    }
+
+    public override void Initialize()
+    {
+        AvaloniaXamlLoader.Load(this);
+    }
+
+    public override void OnFrameworkInitializationCompleted()
+    {
+        if (ApplicationLifetime is IClassicDesktopStyleApplicationLifetime desktop)
+        {
+            desktop.MainWindow = new MainWindow();
+        }
+
+        base.OnFrameworkInitializationCompleted();
+    }
+}
---
+++ Edi.Avalonia/Program.cs
@@ -0,0 +1,50 @@
+﻿using Avalonia;
+using System;
+using System.Threading;
+using Edi.Avalonia;
+using Edi.Core;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Serilog;
+
+var thread = new Thread(() =>
+{
+    using var mutex = new Mutex(true, "Edi", out var createdNew);
+    if (!createdNew)
+    {
+        Environment.Exit(0);
+        return;
+    }
+
+    var host = Host.CreateDefaultBuilder()
+        .UseSerilog((ctx, sp, loggerConfig) =>
+        {
+            var ediConfig = sp.GetService<Edi.Core.Services.ConfigurationManager>().Get<EdiConfig>();
+
+            loggerConfig
+                .MinimumLevel.Debug()
+                .WriteTo.Conditional(
+                    _ => ediConfig.UseLogs,
+                    wt => wt.File(
+                        "./Edilog.txt",
+                        rollingInterval: RollingInterval.Day,
+                        retainedFileCountLimit: 1)
+                );
+        })
+        .ConfigureServices((context, services) =>
+        {
+            services.AddEdi("./EdiConfig.json");
+            services.AddTransient<App>();
+        })
+        .Build();
+
+    host.StartAsync().GetAwaiter().GetResult();
+
+    var app = AppBuilder.Configure<App>(() => host.Services.GetRequiredService<App>()).UsePlatformDetect();
+    app.StartWithClassicDesktopLifetime([]);
+
+    host.StopAsync().GetAwaiter().GetResult();
+    host.Dispose();
+});
+
+thread.Start();
